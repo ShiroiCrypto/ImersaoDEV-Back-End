c49c5ba6db5334bb4e956687148f075d28c4ce2e
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2024-11-20 20:09:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
 
+const posts = [
+    {
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
 app.listen(3000, () => {
     console.log("Servidor escutando...");
