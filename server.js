d4710fe63c13c6691abfeb7fe8c455b09409b5b4
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2024-11-20 20:31:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 const posts = [
-    {
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-    }
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+    { id: 4, descricao: "Gato lendo um livro", imagem: "https://placecats.com/millie/300/150" },
+    { id: 5, descricao: "Gato tomando sol na janela", imagem: "https://placecats.com/millie/300/150" },
+    { id: 6, descricao: "Gato brincando com um novelo de lã", imagem: "https://placecats.com/millie/300/150" }
 ];
 
 const app = express();
@@ -15,5 +17,16 @@ app.listen(3000, () => {
 });
 
 app.get("/posts", (req, res) => {
-    res.status(200).send();
+    res.status(200).json(posts);
+});
+
+function buscarPostID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostID(req.params.id)
+    res.status(200).json(posts[index]);
 });
