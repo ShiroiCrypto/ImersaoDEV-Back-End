df2fc54fbccf9bd92629a02166e22678befc73a9
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2024-11-24 10:41:29 -0300

    v04

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,4 @@
-import { getTodosPosts } from "../models/postsModels.js";
+import { getTodosPosts, criarPost } from "../models/postsModels.js";
 
 export async function listarPosts (req, res)
 {
@@ -8,5 +8,12 @@ export async function listarPosts (req, res)
 
 export async function postarNovoPost(req, res) 
 {
-
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro:": "Falha na requisição."})
+    }
 }
